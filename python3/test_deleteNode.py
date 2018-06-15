d62857a58d75f53a28bfc42f8d38fe453e2fd616
glight <gulight@hotmail.com>
2018-06-15 20:45:25 +0800

    add code file for deleteNode

---
+++ python3/test_deleteNode.py
@@ -0,0 +1,13 @@
+import unittest
+from deleteNode import *
+
+
+class TestFunc(unittest.TestCase):
+    """Test deleteNode.py"""
+    def test_deleteNode(self):
+        t = Solution()
+        # self.assertEqual("olleh", t.deleteNode("hello"))
+
+
+if __name__ == '__main__':
+    unittest.main()
